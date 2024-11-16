6de2bfea66d10538aeacf4c63dade7d7aff19aff
rimadias <yunusovsaidamir21@gmail.com>
2024-11-16 23:22:38 +0500

    даление таблиц минус

--- models/migrations.go
+++ models/migrations.go
@@ -3,10 +3,6 @@ package models
 import "gorm.io/gorm"
 
 func AutoMigrate(db *gorm.DB) error {
-	err := db.Migrator().DropTable(&Company{}, &Driver{}, &Logistic{}, &Employee{})
-	if err != nil {
-		return err
-	}
 	return db.AutoMigrate(
 		&Company{},
 		&Driver{},
