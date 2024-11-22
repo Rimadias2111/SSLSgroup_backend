d77b6006c265fe9aaf22faf5571df7cb1d585e64
rimadias <yunusovsaidamir21@gmail.com>
2024-11-22 21:57:13 +0500

    swag init

--- models/migrations.go
+++ models/migrations.go
@@ -3,10 +3,16 @@ package models
 import "gorm.io/gorm"
 
 func AutoMigrate(db *gorm.DB) error {
+	if err := db.Migrator().DropTable(&Employee{}); err != nil {
+		return err
+	}
 	return db.AutoMigrate(
 		&Company{},
 		&Driver{},
 		&Logistic{},
 		&Employee{},
+		&Cargo{},
+		&Transaction{},
+		&Performance{},
 	)
 }
