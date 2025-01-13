e7776ce1dcce6288982f6bec81c4b9db00f1feb2
rimadias <yunusovsaidamir21@gmail.com>
2025-01-13 20:29:34 +0300

    time zone change

--- etc/helpers/countdown.go
+++ etc/helpers/countdown.go
@@ -13,7 +13,7 @@ func CountDown(resp *models.GetAllLogisticsResp) {
 			logistic := &resp.Companies[i].Logistics[j]
 			switch logistic.Status {
 			case "READY", "AT HOME", "READY AT HOME", "LET US KNOW":
-				logistic.Countdown = now.Sub(logistic.UpdateTime).String()
+				logistic.Countdown = now.Sub(Utime.Parse(logistic.UpdateTime)).String()
 			case "COVERED", "ETA", "ETA WILL BE LATE":
 				logistic.Countdown = logistic.UpdateTime.Sub(now).String()
 			case "AT PU", "AT DEL", "TRUCK ISSUES":
